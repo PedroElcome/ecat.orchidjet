11fc87ec965bfa6cacf185bb5ea14be5d332bc50
pedro.barbosa <pedro.barbosa@elcome.ltd.uk>
2019-07-25 14:44:27 +0100

    Add project files.

---
+++ eCat.OrchidJet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eCat.OrchidJet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
